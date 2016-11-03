7cbcdda8eddd1ff677a7b3a32d1a5c3b1e7e2db5
Grendel Yang <stevenygd@gmail.com>
2016-11-03 19:06:43 -0400

    [debug] Update the fully connected layer to add bias terms. Update Square loss layer to make it 1/2(x-y)^2

--- layers/FCLayer.jl
+++ layers/FCLayer.jl
@@ -2,48 +2,67 @@ include("Criteria.jl")
 
 # Define the Fully Connected layers
 type FCLayer <: Layer
+    i           :: Int64
     W           :: Array{Float64}
     last_input  :: Array{Float64}
     last_output :: Array{Float64}
     last_loss   :: Array{Float64}
     last_diff   :: Array{Float64}
 
-    function FCLayer(i, o)
+    function FCLayer(i, o; init_type = "Uniform")
         # Use Glorot initialization: http://lasagne.readthedocs.io/en/latest/modules/init.html#r5
-        local a = sqrt(2. / (i + o))
-        local newW = rand(i,o)* 2 * a - a
-        return new(newW, zeros(i), zeros(o), zeros(o), zeros(i, o))
+        local newW = zeros(i+1,o)
+        if init_type == "Uniform"
+            local a    = sqrt(12. / (i + o))
+            newW = randn(i+1,o)* 2 * a - a
+        elseif init_type == "Normal"
+            local sigma = sqrt(2. / (i + o))
+            newW  = randn(i+1,o)* 2 * sigma
+        elseif init_type == "Random"
+            newW = rand(i+1,o) - 0.5
+        end
+        # save the original input size
+        return new(i, newW, zeros(i), zeros(o), zeros(o), zeros(i, o))
     end
 end
+
+verbose = 0
 
 function forward(l::FCLayer, X::Array{Float64,2})
     # X      : NxI matrix, N is the mini batch size, I is the input size
     # Output : NxO matrix
-    @assert size(X)[2] == size(l.W)[1]
-    l.last_input  = X
-    l.last_output = X * l.W # generate NxO matrix O is the output size
+    @assert size(X)[2] == l.i
+    # Pad one at the end of the vector
+    l.last_input  = ones(size(X)[1], l.i + 1)
+    l.last_input[:,1:l.i]  = X
+
+    l.last_output = l.last_input * l.W
     return l.last_output
 end
 
 function backward(l::FCLayer, DLDY::Array{Float64,2})
     @assert size(DLDY)[2] == size(l.W)[2]
     l.last_loss = DLDY
-    return DLDY * l.W'
+    return (DLDY * l.W')[:, 1:l.i] # get rid of the bias
 end
 
 function gradient(l::FCLayer)
+    local ret = zeros()
     local g = l.last_input' * l.last_loss
     @assert size(g) == size(l.W)
     return g
 end
 
 function getParam(l::FCLayer)
-    l.W
+    return l.W
 end
 
 function setParam!(l::FCLayer, theta::Array{Float64})
     @assert size(l.W) == size(theta)
     l.last_diff = theta - l.W
+    if verbose > 0
+        println("Difference:$(sum(abs(l.last_diff)))")
+    end
     l.W = theta
 end
 
@@ -52,7 +71,8 @@ function getLDiff(l::FCLayer)
 end
 
 l = FCLayer(2,3)
-l.W = [ 2 3 4 ; 0 9 8 ]
+println(l)
+l.W = [ 2 3 4 ; 0 9 8 ; 1 1 1]
 X = [ 1. 2; 3 4 ]
 Y = [ 5. 6 7 ; 7 8 9 ]
 println(forward(l, X))
--- layers/SquareLossLayer.jl
+++ layers/SquareLossLayer.jl
@@ -1,6 +1,6 @@
 include("Criteria.jl")
 
-type SquareLossLayer <: Layer
+type SquareLossLayer <: LossCriteria
     last_input :: Array{Float64}
     last_output :: Array{Float64}
     function SquareLossLayer()
@@ -8,7 +8,9 @@ type SquareLossLayer <: Layer
     end
 end
 
-function forward(l::SquareLossLayer, Y::Array{Float64,2}, t::Array{Float64, 2})
+verbose = 1
+
+function forward(l::SquareLossLayer, Y::Array{Float64}, t::Array{Float64})
     """
     [label]  label[i] == 1 iff the data is classified to class i
     [y]      final input to the loss layer
@@ -18,23 +20,35 @@ function forward(l::SquareLossLayer, Y::Array{Float64,2}, t::Array{Float64, 2})
     pred: naive prediction that if greater than 0 then 1 else 0
     
     """
-    N = size(Y)[1]
+    N = convert(Float64, size(Y)[1])
     l.last_input = Y
     
     local temp = Y - t
-    local loss = temp' * temp / N
+    local loss = temp' * temp / 2.
     local pred = map(x -> (x > 0)?1:0, Y)    
+    if verbose > 0
+        println("Diff: $(temp)")
+        if verbose > 1
+            println("Input:$(Y), Target:$(t)")
+        end
+    end
     return loss, pred
     
 end
 
-function backward(l::SquareLossLayer, t::Array{Float64, 2})
+function backward(l::SquareLossLayer, t::Array{Float64})
     """
     [label]  label[i] == 1 iff the data is classified to class i
     [y]      final input to the loss layer
     """
     local N = convert(Float64, size(l.last_input)[1])
-    local DLDY = 2 * (l.last_input - t) / N
+    local DLDY = l.last_input - t
+    if verbose > 0
+        println("dldy:$(DLDY)")
+        if verbose > 1
+            println("Input $(l.last_input), Target:$(t)")
+        end
+    end
     return DLDY
 end
 
