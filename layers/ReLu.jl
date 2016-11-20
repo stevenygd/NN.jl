f5136ae0e569790923390897860252175297e066
Grendel Yang <stevenygd@gmail.com>
2016-11-20 17:22:37 -0500

    [opt] Add Relu optimiaztion make all arrays reallocated.

--- layers/ReLu.jl
+++ layers/ReLu.jl
@@ -14,17 +14,29 @@ type ReLu <: Nonlinearity
     end
 end
 
-function forward(l::ReLu, X::Array{Float64}; kwargs...)
-    l.last_input  = X
-    l.last_output = max(X, 0.) * l.alpha
-    l.last_output
+function forward(l::ReLu, X::Union{SubArray{Float64},Array{Float64}}; kwargs...)
+    if size(l.last_input, 1)  != size(X, 1) ||
+       size(l.last_output, 1) != size(X, 1)
+       l.last_input = Array{Float64}(size(X))
+       l.last_output = Array{Float64}(size(X))
+    end
+    l.last_input = X
+    broadcast!(max, l.last_output, X, 0.)
+    broadcast!(*,   l.last_output, l.last_output, l.alpha)
+    return l.last_output
 end
 
-function backward(l::ReLu, DLDY::Array{Float64}; kwargs...)
+function backward(l::ReLu, DLDY::Union{SubArray{Float64},Array{Float64}}; kwargs...)
     @assert size(l.last_input) == size(DLDY)
-    l.last_loss = DLDY .* map(x -> x > 0 ? l.alpha : 0.0, l.last_input)
+    if size(l.last_loss, 1) != size(DLDY, 1)
+        l.last_loss = Array{Float64}(size(DLDY))
+    end
+    broadcast!(>, l.last_loss, l.last_input, 0.)        # l.last_loss = l.last_input .> 0.
+    broadcast!(*, l.last_loss, l.last_loss, l.alpha)    # l.last_loss = l.last_loss * alpha
+    broadcast!(*, l.last_loss, l.last_loss, DLDY)
     return l.last_loss
 end
+
 
 function gradient(l::ReLu)
     0
@@ -42,8 +54,25 @@ function getLDiff(l::ReLu)
     0
 end
 
-#l = ReLu()
-#X = [ 1. 2; -1 3; 1 -2; -3 -3]
-#Y = [ 2. 3; 2 5; 3 6; 2 2]
-#println(forward(l, X))
-#println(backward(l, Y))
+#
+# l = ReLu()
+# X = rand(1000, 500)
+# Y = rand(1000, 500)
+# println("Compile the method for the first time...")
+# forward(l,X)
+# backward(l,Y)
+#
+# println("Start profiling...")
+# print("Forward:")
+# @time begin
+#   for i = 1:1000
+#     forward(l,X)
+#   end
+# end
+#
+# print("Backward")
+# @time begin
+#   for i = 1:1000
+#     backward(l,Y)
+#   end
+# end
