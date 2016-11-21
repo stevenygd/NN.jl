7fb52533094901fba3f8bd42e368bf514f14df8c
Grendel Yang <stevenygd@gmail.com>
2016-11-21 15:26:32 -0500

    [opt] Try to preallocate memory for the SoftmaxCrossEntropy layer.

--- layers/SoftMaxCrossEntropy.jl
+++ layers/SoftMaxCrossEntropy.jl
@@ -4,8 +4,14 @@ type SoftMaxCrossEntropyLoss <: LossCriteria
     last_loss   :: Array{Float64}
     last_input  :: Array{Float64}
     last_output :: Array{Float64}
+    tmpY        :: Array{Float64}
+    loss        :: Array{Float64}
+    pred        :: Array{Int64}
+    label       :: Array{Int64}
+
     function SoftMaxCrossEntropyLoss()
-        return new(Float64[], Float64[])
+        return new(Float64[], Float64[], Float64[], Float64[], Float64[],
+                   Int64[], Int64[])
     end
 end
 
@@ -14,16 +20,29 @@ function forward(l::SoftMaxCrossEntropyLoss, Y::Array{Float64,2}, label::Array{F
     [label]  label[i] == 1 iff the data is classified to class i
     [y]      final input to the loss layer
     """
-    N = size(Y)[1]
+    N = size(Y, 1)
+    if  size(l.last_input, 1) != N || size(l.last_output, 1) != N
+        l.last_loss   = Array{Float64}(size(Y))
+        l.last_input  = Array{Float64}(size(Y))
+        l.last_output = Array{Float64}(size(Y))
+        l.tmpY        = Array{Float64}(N, 1)
+        l.loss        = Array{Float64}(N)
+        l.pred        = Array{Int64}(N)
+        l.label       = Array{Int64}(N)
+    end
+
     l.last_input = Y
-    Y = broadcast(+, Y, - maximum(Y, 2))
-    l.last_output = broadcast(+, -Y, log(sum(exp(Y), 2)))
+    maximum!(l.tmpY, Y)
+    broadcast!(-, l.last_output, l.last_input, l.tmpY)
+    sum!(l.tmpY, exp(l.last_output))
+    broadcast!(+, l.last_output, log(l.tmpY), l.last_output)
 
     @assert size(l.last_output) == size(Y)
-    label = map(x -> convert(Int64, x) + 1, label)
-    local loss = map(i -> l.last_output[i, label[i]], 1:N)
-	local pred = map(i -> findmax(Y[i,:])[2] - 1, 1:N)
-    return loss, pred
+    map!(x -> convert(Int64, x) + 1,        l.label, label)
+    map!(i -> l.last_output[i, l.label[i]], l.loss,  1:N)
+    map!(i -> findmax(Y[i,:])[2] - 1,       l.pred,  1:N)
+
+    return l.loss, l.pred
 end
 
 function backward(l::SoftMaxCrossEntropyLoss, label::Array{Float64, 2};kwargs...)
@@ -47,17 +66,27 @@ function backward(l::SoftMaxCrossEntropyLoss, label::Array{Float64, 2};kwargs...
     return Y .- TAR
 end
 
-# l = SoftMaxCrossEntropyLoss()
-# lbl = map(x -> convert(Float64, x), rand(0:9,10))
-# println(size(lbl))
-# println(lbl)
-# println(forward(l, rand(5,10), lbl))
-# println(backward(l, lbl))
-# y = zeros(2,1)
-# y[1] = 1
-# y[2] = 2
-# x = [ 2. 1. 3;
-#     -2. 3. 2.]
-#loss, pred = forward(l, x, y)
-#println((loss, pred))
-#println(backward(l,y))
+l = SoftMaxCrossEntropyLoss()
+X = rand(500, 10) #input size 784, batch size 500
+L = map(x -> ceil(x), rand(500, 1))
+
+println("First time (compiling...)")
+@time forward(l,X,L)
+@time backward(l,L)
+
+println("Second time ...")
+@time forward(l,X,L)
+@time backward(l,L)
+
+
+println("Third time (profiling...)")
+@time begin
+  for i = 1:1000
+    forward(l,X,L)
+  end
+end
+@time begin
+  for i = 1:1000
+    backward(l,L)
+  end
+end
