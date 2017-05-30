4441fd6b57eaa7ad368dd170ee25036a6d8fb6c1
fufjvnvnf <fufjvnvnf@gmail.com>
2017-05-30 23:17:17 +0800

    somehow this wasn't pushed

--- src/layers/SoftMax.jl
+++ src/layers/SoftMax.jl
@@ -1,41 +1,54 @@
 type SoftMax <: Nonlinearity
-    last_input  :: Array{Float64}
-    last_output :: Array{Float64}
-    last_loss   :: Array{Float64}
+    x  :: Array{Float64}
+    y :: Array{Float64}
+    # last_loss   :: Array{Float64}
 
     function SoftMax()
-        return new(Float64[], Float64[], Float64[])
+        return new(Float64[], Float64[])
     end
 end
 
 function forward(l::SoftMax, X::Array{Float64}; kwargs...)
-	X = broadcast(+, X, -maximum(X))
-    l.last_input = X
+	  # X = broadcast(+, X, -maximum(X))
+    l.x = X
+    x_exp = exp(X)
     sumX = sum(exp(X))
-    Y = (x ./ sumX)
-    l.last_output = Y
+    Y = (x_exp ./ sumX)
+    @assert size(Y) == size(X)
+    l.y = Y
     return Y
-
 end
 
 function backward(l::SoftMax, DLDY::Array{Float64}; kwargs...)
-    @assert size(l.last_input) == size(DLDY)
-    X = l.last_input
-    sumX = sum(exp(X))
-    u = zeros(ndims(X), ndims(X))
-    z = zeros(ndims(X))
-    for i = 1: ndims(X)
-        z[i] = (X[i]/sumX)
+    @assert size(l.x) == size(DLDY)
+    # credits: https://stats.stackexchange.com/questions/79454/softmax-layer-in-a-neural-network?newreg=d1e89b443dd346ae8bccaf038a944221
+    X = l.x
+    jacobian = zeros(size(l.x)[1], size(l.x)[1])
+    println(typeof(jacobian))
+    for i=1:size(l.x)[1]
+      for j=1:size(l.x)[1]
+        if i==j
+          jacobian[i,j] = l.x[j]*(1.0-l.x[j])
+        else
+          jacobian[i,j] = -l.x[i]*l.x[j]
+        end
+      end
     end
-
-    for i = 1: ndims(X)
-        t = z[i]
-        w = zeros(ndims(X))
-        w[i] = 1
-        w = w .- z
-        u[:,i] = t * w
-    end
-    l.last_loss = DLDY' * u
-    return l.last_loss
+    return jacobian * DLDY
+    # sumX = sum(exp(X))
+    # u = zeros(ndims(X), ndims(X))
+    # z = zeros(ndims(X))
+    # for i = 1: ndims(X)
+    #     z[i] = (X[i]/sumX)
+    # end
+    #
+    # for i = 1: ndims(X)
+    #     t = z[i]
+    #     w = zeros(ndims(X))
+    #     w[i] = 1
+    #     w = w .- z
+    #     u[:,i] = t * w
+    # end
+    # l.last_loss = DLDY' * u
 
 end
