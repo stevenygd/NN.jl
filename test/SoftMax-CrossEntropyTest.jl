86391cd0c3ff1614623a20dc5a471e8d8a186621
fufjvnvnf <fufjvnvnf@gmail.com>
2017-06-12 01:25:20 +0800

    softmax + cross entropy passed initial test

--- test/SoftMax-CrossEntropyTest.jl
+++ test/SoftMax-CrossEntropyTest.jl
@@ -3,3 +3,37 @@ include("../src/layers/SoftMaxCrossEntropy.jl")
 include("../src/layers/SoftMax.jl")
 include("../src/layers/CrossEntropyLoss.jl")
 using Base.Test
+
+smaxcross = SoftMaxCrossEntropyLoss()
+smax = SoftMax()
+cross = CrossEntropyLoss()
+
+
+function beforeTest(smaxcross, smax, cross)
+    init(smaxcross, nothing, Dict{String, Any}("batch_size" => 2, "input_size" => [3]))
+    init(smax     , nothing, Dict{String, Any}("batch_size" => 2, "input_size" => [3]))
+    init(cross    , nothing, Dict{String, Any}("batch_size" => 2, "input_size" => [3]))
+end
+
+function testSoftMaxCrossEntropyOneVector(smaxcross, smax, cross, x, label; alpha = 1.)
+    beforeTest(smaxcross, smax, cross)
+
+    # Testing forwarding
+    l1, p1 = forward(smaxcross,x,y)
+    l2, p2 = forward(cross, forward(smax,x), label)
+    @test_approx_eq l1 l2
+    @test_approx_eq p1 p2
+    d1 = backward(smaxcross,label)
+    d2 = backward(smax, backward(cross, label))
+
+    # Testing back propagation
+    @test_approx_eq d1 d2
+end
+
+# First Test
+println("Unit test 1...")
+x = [1. 2. 3.; -1. -2. -3.]
+y = zeros(Int64, 2,1)
+y[:, 1] = [2      ;   1]
+testSoftMaxCrossEntropyOneVector(smaxcross, smax, cross, x, y; alpha = 1.)
+println("test 1 passed.\n")
