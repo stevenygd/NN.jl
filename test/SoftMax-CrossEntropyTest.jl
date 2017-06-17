050993fdc849a69d772d485834e455d5014140be
fufjvnvnf <fufjvnvnf@gmail.com>
2017-06-18 02:05:26 +0800

    refractored out benchmark into benchmark pkg

--- test/SoftMax-CrossEntropyTest.jl
+++ test/SoftMax-CrossEntropyTest.jl
@@ -30,65 +30,10 @@ function testSoftMaxCrossEntropyOneVector(smaxcross, smax, cross, x, label; alph
     @test_approx_eq d1 d2
 end
 
-function benchmmark(smaxcross, smax, cross, batch_size, input_size; alpha = 1.)
-  # batch_size = 10
-  # input_size = 5
-  beforeTest(smaxcross, smax, cross, Dict{String, Any}("batch_size" => batch_size, "input_size" => [input_size]))
-  x = rand(batch_size, input_size)
-  label = rand(0:input_size-1, batch_size,1)
-  # benchmark for original SoftMaxCrossEntropy
-  tic()
-
-  # l1, p1 = @time forward(smaxcross,x,label)
-  l1, p1 = forward(smaxcross,x,label)
-
-  time = toq()
-  println("old forward uses:  ", time)
-  tic()
-
-  # d1 = @time backward(smaxcross,label)
-  d1 = backward(smaxcross,label)
-
-  time = toq()
-  println("old backward uses: ", time)
-
-  # benchmark for newly implemented softmax & CrossEntropyLoss
-  tic()
-
-  # l2, p2 = @time forward(cross, forward(smax,x), label)
-  l2, p2 = forward(cross, forward(smax,x), label)
-
-  time = toq()
-  println("new forward uses:  ", time)
-  tic()
-
-  # d2 = @time backward(smax, backward(cross, label))
-  d2 = backward(smax, backward(cross, label))
-
-  time = toq()
-  println("new backward uses: ", time)
-
-  @test_approx_eq l1 l2
-  @test_approx_eq p1 p2
-  println("Correctness test passed")
-end
-
 # First Test
-# println("Unit test 1...")
-# x = [1. 2. 3.; -1. -2. -3.]
-# y = zeros(Int64, 2,1)
-# y[:, 1] = [2      ;   1]
-# testSoftMaxCrossEntropyOneVector(smaxcross, smax, cross, x, y; alpha = 1.)
-# println("basic test passed.\n")
-
-# for i=1:20
-#   benchmmark(smaxcross, smax, cross, 10, 5; alpha = 1.)
-# end
-#
-# for i=1:20
-#   benchmmark(smaxcross, smax, cross, 100, 10; alpha = 1.)
-# end
-
-for i=1:20
-  benchmmark(smaxcross, smax, cross, 1000, 100; alpha = 1.)
-end
+println("Unit test 1...")
+x = [1. 2. 3.; -1. -2. -3.]
+y = zeros(Int64, 2,1)
+y[:, 1] = [2      ;   1]
+testSoftMaxCrossEntropyOneVector(smaxcross, smax, cross, x, y; alpha = 1.)
+println("basic test passed.\n")
