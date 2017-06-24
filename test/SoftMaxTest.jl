3ad9c5f08f76e068651d813916614c9cece63b03
fufjvnvnf <fufjvnvnf@gmail.com>
2017-06-24 10:43:42 +0800

    added solid tests for softamx

--- test/SoftMaxTest.jl
+++ test/SoftMaxTest.jl
@@ -4,24 +4,54 @@ using Base.Test
 using ForwardDiff
 
 l = SoftMax();
-s = x->x->exp(x)./sum(exp(x))
-g = x->ForwardDiff.gradient(s, x)
+s(x::Vector) = exp(x)./sum(exp(x))
+g(x::Vector) = ForwardDiff.jacobian(s,x)
 
-function before(l)
-    init(l, nothing, Dict{String, Any}("batch_size" => 1, "input_size" => [3]))
+function before(l,x)
+    m,n = size(x)
+    init(l, nothing, Dict{String, Any}("batch_size" => m, "input_size" => [n]))
 end
 
-function testSoftMax(x::Array{Float64}, y::Array{Float64}, dldy::Array{Float64}, dldx::Array{Float64}; alpha = .001)
+function testSoftMax(x::Array{Float64}, y::Array{Float64}, dldy::Array{Float64}, dldx::Array{Float64}; alpha = 1.)
   # Testing forwarding
-  before(l)
-  @test_approx_eq forward(l,x) exp(x)./sum(exp(x))
-  @test_approx_eq backward(l,dldy) dldx
+  before(l,x)
+  f = vec(forward(l,x))
+  b = vec(backward(l,dldy))
+  @test_approx_eq_eps norm(f-y) 0 alpha
+  @test_approx_eq_eps norm(b-dldx) 0 alpha
 
 end
 
-# First Test
+
+# Basic Test
 println("Unit test 1...")
-x = reshape([1.0, 2.0, 3.0;], 1,3)
-dldy = reshape([1. 1. 1.;], 1,3)
-testSoftMax(x, s(x), dldy, g(x))
-println("test 1 passed.\n")
+x = rand(1,3)
+dldy = ones(1,3)
+dldy_v = [1., 1., 1.]
+testSoftMax(x, s(vec(x)), dldy, g(vec(x))*dldy_v)
+println("Basic test passed.\n")
+
+# Number < 30 with 10 classes
+println("Unit test 2...")
+x = 30*rand(1,10)
+dldy = ones(1,10)
+dldy_v = vec(dldy)
+testSoftMax(x, s(vec(x)), dldy, g(vec(x))*dldy_v)
+println("Complicated test passed.\n")
+
+# Big number test
+println("Unit test 3...")
+x = 692*rand(1, 100)
+dldy = ones(1,100)
+dldx = g(vec(x))*vec(dldy)
+testSoftMax(x, s(vec(x)), dldy, dldx)
+println("Big number test passed.\n")
+
+# Big big number big classes test
+println("Unit test 4...")
+x = 678*rand(1,1000)
+x_v = vec(x)
+dldy = ones(1,1000)
+dldy_v = vec(dldy)
+testSoftMax(x, s(x_v), dldy, g(x_v)*dldy_v)
+println("Big big number test passed.\n")
