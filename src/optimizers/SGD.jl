0eb24f52f187c44a8d051a438be8dcf02c42a28e
mawenyuan1997 <wm263@cornell.edu>
2017-07-24 17:26:31 +0800

    SGD Optimizer

---
+++ src/optimizers/SGD.jl
@@ -0,0 +1,35 @@
+
+type SgdOptimizer
+    net     :: SequentialNet
+    base_lr :: Any
+    iter    :: Int
+    batch_size  ::  Int
+
+    function SgdOptimizer(net::SequentialNet, batch_size;  base_lr=(x->0.01))
+         return new(net, base_lr, 1, batch_size)
+    end
+ end
+
+ function optimize(this::SgdOptimizer, batch_X, batch_Y)
+
+     loss, pred = forward(this.net, batch_X, batch_Y)
+     backward(this.net, batch_Y)
+
+     for i = 1:length(this.net.layers)
+         layer = this.net.layers[i]
+
+         param = getParam(layer)
+         if param == nothing
+             continue
+         end
+
+         gradi = getGradient(layer)
+         for j = 1:length(gradi)
+             param[j] -= this.base_lr(this.iter) * gradi[j] / this.batch_size
+         end
+         setParam!(layer, param)
+     end
+
+     this.iter += 1;
+     return loss, pred
+ end
---
+++ test/SgdOptimizerTest.jl
@@ -0,0 +1,57 @@
+include("../src/NN.jl")
+include("../src/layers/LayerBase.jl")
+include("../src/layers/DenseLayer.jl")
+include("../src/layers/Sigmoid.jl")
+include("../src/layers/ReLu.jl")
+include("../src/layers/SquareLossLayer.jl")
+include("../src/layers/SoftMaxCrossEntropy.jl")
+include("../src/layers/SequentialNet.jl")
+include("../src/layers/InputLayer.jl")
+
+using NN
+
+function OptimizerTest(net::SequentialNet, train_set, optimizer;
+    batch_size::Int64 = 4, ttl_epo::Int64 = 1000)
+
+    batch_X, batch_Y = train_set
+    batch_X = reshape(batch_X, 4, 2)
+
+    for epo = 1:ttl_epo
+          loss, pred = optimize(optimizer, batch_X, batch_Y)
+          println("Epo $(epo) has loss :$(loss)\t prediction is $(net.lossfn.x)")
+          println("         layer1: W = $(net.layers[2].W)")
+          println("         layer2: W = $(net.layers[4].W)")
+          #println("         layer2: y = $(net.layers[2].y)")
+    end
+    return
+end
+# build the layer
+l1 = DenseLayer(2)
+init(l1, nothing, Dict{String, Any}("batch_size" => 4, "input_size" => [2]))
+w = [0.5 0.5;
+     0.5 0.5;]
+b = [0. 0;]
+W = [w; b;]
+setParam!(l1, Array[W])
+
+l2 = DenseLayer(1)
+init(l2, l1, Dict{String, Any}("batch_size" => 4, "input_size" => [2]))
+w = [ 1.;
+      1.;]
+b = [0.;]
+W = reshape([w; b;], 3, 1)
+print(size(l2.W))
+setParam!(l2, Array[W])
+# training set
+trX = [0. 0.;
+       0. 1.;
+       1. 0.;
+       1. 1.;]
+trY = [0., 1., 0., 1.]
+# build the net
+layers = Layer[InputLayer((4,2)), l1, ReLu(), l2, ReLu()]
+criteria = SquareLossLayer()
+net = SequentialNet(layers, criteria)
+# test SGD
+optimizer = SgdOptimizer(net, 1 , base_lr=(x->0.01))
+OptimizerTest(net, (trX, trY), optimizer, batch_size = 4)
