dc399ef50b45d55e7472474650f13ea66137e318
Tiiiger <zty27x@gmail.com>
2017-09-20 14:02:31 -0400

    "Finish the example with SGD optimizer."

--- examples/graph_example.jl
+++ examples/graph_example.jl
@@ -28,10 +28,15 @@ function build_graph()
     return graph
 end
 
+function get_corr(pred, answ)
+    return length(filter(e -> abs(e) < 1e-5, pred-answ))
+end
+
 function sgd(graph::Graph, train_set, validation_set;
     batch_size::Int64 = 100, ttl_epo::Int64 = 10, lrSchedule = (x -> 0.01), alpha::Float64 = 0.9, verbose=0)
     X, Y = train_set
     valX, valY = validation_set
+    optimizer = SGDOptimizerGraph(graph, batch_size=batch_size, base_lr=lrSchedule)
     local N = size(Y)[1]
     local batch=0
     local epo_losses = []
@@ -53,24 +58,7 @@ function sgd(graph::Graph, train_set, validation_set;
                 local eidx::Int = convert(Int64, min(N, (bid+1)*batch_size))
                 local batch_X = X[:,:,:,sidx:eidx]
                 local batch_Y = Y[sidx:eidx,:]
-                loss, pred = forward(net, batch_X, batch_Y)
-                backward(net, batch_Y)
-                append!(all_losses, mean(loss))
-                for i = 1:length(net.layers)
-                    layer = net.layers[i]
-
-                    param = getParam(layer)
-                    if param == nothing
-                        continue
-                    end
-
-                    gradi = getGradient(layer)
-                    for j = 1:length(gradi)
-                        param[j] -= lrSchedule(epo) * gradi[j] / batch_size
-                    end
-                    setParam!(layer, param)
-                end
-
+                loss, pred = optimize(optimizer, batch_X, batch_Y)
                 # _, pred = forward(net, batch_X, batch_Y; deterministics = true)
                 epo_cor  += get_corr(pred, batch_Y)
                 local acc = get_corr(pred, batch_Y) / batch_size
@@ -83,7 +71,7 @@ function sgd(graph::Graph, train_set, validation_set;
         append!(epo_accus, epo_accu)
 
         # Run validation set
-        v_ls, v_pd = forward(net, valX, valY)
+        v_ls, v_pd = forward(graph, valX, valY)
         local v_loss = mean(v_ls)
         v_size = size(valX)[1]
         v_accu = get_corr(v_pd, valY) / v_size
@@ -96,3 +84,36 @@ function sgd(graph::Graph, train_set, validation_set;
     end
     return epo_losses,epo_accus, val_losses, val_accu, all_losses
 end
+
+X,Y = mnistData(ttl=55000) # 0-1
+println("X statistics: $(mean(X)) $(minimum(X)) $(maximum(X))")
+
+Y = round(Int, Y)
+train_set, test_set, validation_set = datasplit(X,Y;ratio=10./11.)
+trX, trY = train_set[1], train_set[2]
+valX, valY = validation_set[1], validation_set[2]
+teX, teY = test_set[1], test_set[2]
+
+trX  = permutedims(reshape(trX,  (size(trX,1),  1, 28, 28)), [3,4,2,1])
+valX = permutedims(reshape(valX, (size(valX,1), 1, 28, 28)), [3,4,2,1])
+teX  = permutedims(reshape(teX,  (size(teX,1),  1, 28, 28)), [3,4,2,1])
+
+println("TrainSet: $(size(trX)) $(size(trY))")
+println("ValSet  : $(size(valX)) $(size(valY))")
+println("TestSet : $(size(teX)) $(size(teY))")
+
+graph = build_graph()
+
+epo_losses, epo_accu, val_losses, val_accu, all_losses = sgd(
+    net, (trX, trY), (valX, valY);
+    ttl_epo = 10, batch_size = batch_size,
+    lrSchedule = x -> 0.01, verbose=1
+)
+
+figure(figsize=(12,6))
+# subplot(221)
+plot(1:length(all_losses), all_losses)
+title("SGD Graph: Training losses")
+
+show()
+
