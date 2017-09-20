36de77bf36680e2076725336b8c808fe5545b8c2
fufjvnvnf <fufjvnvnf@gmail.com>
2017-09-20 13:59:08 -0400

    initial implementation of topsort

--- src/layers/Graph.jl
+++ src/layers/Graph.jl
@@ -1,24 +1,26 @@
 type Graph <: ANN
     layer :: Layer
     forward_order :: Array{Layer}
+    visited :: Set{Layer}
 
     function Graph(layer::Layer)
         # Initialize all the layers
-        graph = new(layers)
-        top_sort()
+        graph = new(layer)
+        graph.visited = Set{Layer}([graph.layer])
+        top_sort(graph, graph.layer)
         return graph
     end
 end
 
-function top_sort(graph::Graph)
-    l = graph.layer
-    visited = Set([l])
-    frontier = [l]
-    while length(l) > 0
-        layer = frontier[0]
-        
+function top_sort(graph::Graph, layer::Layer)
+    push!(graph.visited, layer)
+    for i=1:length(layer.parents)
+        l = layer.parent[i]
+        if !in(l, graph.visited)
+            top_sort(graph, l)
+        end
+        append!(graph.forward_order, l)
     end
-    return Layer[] # return an array of layer
 end
 
 function forward(graph::Graph)
