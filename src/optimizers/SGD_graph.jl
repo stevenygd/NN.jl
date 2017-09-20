7b1787986dfab99ef641100edbb387a6cad82d29
Tiiiger <zty27x@gmail.com>
2017-09-20 13:47:57 -0400

    "Create new Optimizer for the graph interface."

---
+++ src/optimizers/SGD_graph.jl
@@ -0,0 +1,34 @@
+type SgdOptimizerGraph
+    graph     :: Graph
+    base_lr :: Any
+    iter    :: Int
+    batch_size  ::  Int
+
+    function SgdOptimizer(graph::Graph, batch_size;  base_lr=(x->0.01))
+         return new(net, base_lr, 1, batch_size)
+    end
+ end
+
+ function optimize(this::SgdOptimizer, batch_X, batch_Y)
+
+     loss, pred = forward(this.graph, batch_X, batch_Y)
+     backward(this.graph, batch_Y)
+
+     for i = 1:length(this.graph.forward_order)
+         layer = this.graph.forward_order[i]
+
+         param = getParam(layer)
+         if param == nothing
+             continue
+         end
+
+         gradi = getGradient(layer)
+         for j = 1:length(gradi)
+             param[j] -= this.base_lr(this.iter) * gradi[j] / this.batch_size
+         end
+         setParam!(layer, param)
+     end
+
+     this.iter += 1;
+     return loss, pred
+ end
