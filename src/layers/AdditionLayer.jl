775fef0246823928738e3411b64367fda16c5307
Tiiiger <zty27x@gmail.com>
2017-11-07 17:50:03 -0500

    "Change AdditionLayer to meet graph forward interface."

--- src/layers/AdditionLayer.jl
+++ src/layers/AdditionLayer.jl
@@ -4,17 +4,17 @@ type AdditionLayer <: Layer
     children :: Array{Layer}
     has_init :: Bool
 
-    xs    :: Array{Float64}
+    xs    :: Array{Array{Float64}, 1}
     y     :: Array{Float64}
+    dldxs :: Array{Array{Float64}, 1}
     dldy  :: Array{Float64}
-    dldxs :: Array{Float64}
 
     function AdditionLayer()
         return new(Layer[], Layer[], false, Float64[], Float64[], Float64[], Float64[])
     end
 
     function AdditionLayer(prevs::Array{<:Layer}, config::Dict{String, Any})
-        layer =  new(Layer[], Layer[], false, Float64[], Float64[], Float64[], Float64[])
+        layer =  new(Layer[], Layer[], false, Array{Float64}[], Float64[], Array{Float64}[], Float64[])
         init(layer, prevs, config)
         layer
     end
@@ -29,13 +29,10 @@ function init(l::AdditionLayer, ps::Union{Array{<:Layer}}, config::Dict{String, 
     end
 
     parents_size = length(ps)
-    size_array = [i for i in out_size] 
-    push!(size_array, parents_size)
-    size_tuple = Tuple(size_array)
 
-    l.xs = Array{Float64}(size_tuple)
+    l.xs = Array{Array{Float64, length(out_size)}, 1}()
     l.y = Array{Float64}(out_size)
-    l.dldxs = Array{Float64}(size_tuple)
+    l.dldxs = Array{Array{Float64, length(out_size)}, 1}()
     l.dldy = Array{Float64}(out_size)
 
     l.has_init = true
@@ -43,37 +40,43 @@ end
 
 function update(l::AdditionLayer, input_size::Tuple;)
     parents_size = length(l.parents)
-    size_array = [i for i in input_size] 
-    push!(size_array, parents_size)
-    size_tuple = Tuple(size_array)
 
-    l.xs = Array{Float64}(size_tuple)
+    l.xs = Array{Array{Float64, length(input_size)}, 1}()
     l.y = Array{Float64}(input_size)
-    l.dldxs = Array{Float64}(size_tuple)
+    l.dldcs = Array{Array{Float64, length(input_size)}, 1}()
     l.dldy = Array{Float64}(input_size)
 end
 
-function forward(l::AdditionLayer, xs::Union{SubArray{Float64}, Array{Float64}}; kwargs...)
-    if size(xs) != size(l.xs)
-        update(l, size(xs[1]))
-    end
-    l.xs = xs
+function forward(l::AdditionLayer;kwargs...)
+    xs = [l.x for l in l.parents]
     l.y = zeros(l.y)
-    println(l.y)
-    for i in 1:size(l.xs)[3]
-        x = xs[:, :, i]
-        broadcast!(+, l.y, l.y, x)
-        println(l.y)
+    for i=1:size(xs)[1]
+        broadcast!(+, l.y, l.y, xs[i])
     end
     return l.y
 end
 
+# function forward(l::AdditionLayer, xs::Union{SubArray{Float64}, Array{Float64}}; kwargs...)
+#     if size(xs) != size(l.xs)
+#         update(l, size(xs[1]))
+#     end
+#     l.xs = xs
+#     l.y = zeros(l.y)
+#     println(l.y)
+#     for i in 1:size(l.xs)[3]
+#         x = xs[:, :, i]
+#         broadcast!(+, l.y, l.y, x)
+#         println(l.y)
+#     end
+#     return l.y
+# end
+
 function backward(l::AdditionLayer, DLDY::Union{Array{Float64}, SubArray{Float64}};)
     @assert size(l.dldy) == size(DLDY)
-    ndldxs = zeros(l.dldxs)
+    ndldxs = Array{Array{Float64, length(size(l.dldy))}, 1}()
     l.dldy = DLDY
-    for n in 1:size(l.dldxs)[3]
-        ndldxs[:, :, n] = l.dldy
+    for n in 1:size(l.parents)[1]
+        push!(ndldxs, dldy)
     end
     l.dldxs = ndldxs
     return l.dldxs
--- test/AdditionLayerTest.jl
+++ test/AdditionLayerTest.jl
@@ -1,4 +1,5 @@
 include("../src/layers/LayerBase.jl")
+include("../src/layers/Graph.jl")
 include("../src/layers/AdditionLayer.jl")
 include("../src/layers/InputLayer.jl")
 using Base.Test
@@ -8,19 +9,23 @@ function Test(xs, y, dldy)
     input1 = InputLayer(nothing, (1, 2), config)
     input2 = InputLayer(nothing, (1, 2), config)
     l = AdditionLayer([input1, input2], config)
+    forward(input1, xs[1])
+    forward(input2, xs[2])
 
-    @test forward(l, xs) == y
-    @test backward(l, dldy)[:, :, 1] == dldx1
-    @test backward(l, dldy)[:, :, 2] == dldx2
+    @test forward(l) == y
+    @test backward(l, dldy)[1] == dldx1
+    @test backward(l, dldy)[2] == dldx2
 end
 
-xs = Array{Float64}(1, 2, 2)
+xs = Array{Float64}[]
 x1 = [1. 2.;]
 x2 = [1. 1.;]
-xs[:, :, 1] = x1
-xs[:, :, 2] = x2
+push!(xs, x1)
+push!(xs, x2)
+
 y = [2. 3.;]
 dldy = [3. 4.;]
 dldx1 = [3. 4.;]
 dldx2 = [3. 4.;]
 Test(xs, y, dldy)
+println("AdditionLayer Test passed")
