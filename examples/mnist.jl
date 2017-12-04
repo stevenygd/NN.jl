bfc8064e882eb60f8fd9cdc2121a9b59b3703233
Tiiiger <zty27x@gmail.com>
2017-12-03 21:37:57 -0500

    debugging

--- examples/mnist.jl
+++ examples/mnist.jl
@@ -1,29 +1,31 @@
-include("src/NN.jl")
-include("util/datasets.jl")
-
+include("../src/NN.jl")
+include("../util/datasets.jl")
+using NN
 batch_size = 500
 
 function build_model()
-    input = InputLayer((batch_size,784)),
-    l1    = DenseLayer(input, 1000; init_type = "Normal"),
-    l2    = ReLu(l1),
-    l3    = DropoutLayer(l2, 0.5),
-    l4    = DenseLayer(l3, 1000; init_type = "Normal" ),
-    l5    = ReLu(l4),
-    l6    = DropoutLayer(l5, 0.5),
+    input = InputLayer((batch_size,784))
+    loss  = InputLayer((batch_size, 10))
+    l1    = DenseLayer(input, 1000; init_type = "Normal")
+    l2    = ReLu(l1)
+    l3    = DropoutLayer(l2, 0.5)
+    l4    = DenseLayer(l3, 1000; init_type = "Normal" )
+    l5    = ReLu(l4)
+    l6    = DropoutLayer(l5, 0.5)
     l7    = DenseLayer(l6, 10; init_type = "Normal")
-    return Graph(l7)
+    return input, loss, Graph(l7)
 end
 
 function get_corr(pred, answ)
     return length(filter(e -> abs(e) < 1e-5, pred-answ))
 end
 
-function sgd(graph::Graph, train_set, validation_set;
-    batch_size::Int64 = 100, ttl_epo::Int64 = 10, lrSchedule = (x -> 0.01), alpha::Float64 = 0.9, verbose=0)
+function sgd(graph::Graph, layerX::Layer, layerY::Layer, optimizer::SgdOptimizerGraph,
+    train_set, validation_set;
+    batch_size::Int64 = 100, ttl_epo::Int64 = 10,
+    lrSchedule = (x -> 0.01), alpha::Float64 = 0.9, verbose=0)
     X, Y = train_set
     valX, valY = validation_set
-    optimizer = SGDOptimizerGraph(graph, batch_size=batch_size, base_lr=lrSchedule)
     local N = size(Y)[1]
     local batch=0
     local epo_losses = []
@@ -43,9 +45,9 @@ function sgd(graph::Graph, train_set, validation_set;
                 batch += 1
                 local sidx::Int = convert(Int64, bid*batch_size+1)
                 local eidx::Int = convert(Int64, min(N, (bid+1)*batch_size))
-                local batch_X = X[:,:,:,sidx:eidx]
-                local batch_Y = Y[sidx:eidx,:]
-                loss, pred = optimize(optimizer, batch_X, batch_Y)
+                local batch_X = X[sidx:eidx, :]
+                local batch_Y = Y[sidx:eidx, :]
+                loss, pred = optimize(optimizer, Dict(layerX=>batch_X, layerY=>batch_Y))
                 # _, pred = forward(net, batch_X, batch_Y; deterministics = true)
                 epo_cor  += get_corr(pred, batch_Y)
                 local acc = get_corr(pred, batch_Y) / batch_size
@@ -72,27 +74,31 @@ function sgd(graph::Graph, train_set, validation_set;
     return epo_losses,epo_accus, val_losses, val_accu, all_losses
 end
 
+function convert_to_one_hot(x::Array{Int64}, classes)
+  m = zeros(size(x,1), classes)
+  for i=1:size(x,1)
+    m[i,x[i]+1]=1
+  end
+  m
+end
+
 X,Y = mnistData(ttl=55000) # 0-1
 println("X statistics: $(mean(X)) $(minimum(X)) $(maximum(X))")
 
-Y = round(Int, Y)
 train_set, test_set, validation_set = datasplit(X,Y;ratio=10./11.)
 trX, trY = train_set[1], train_set[2]
 valX, valY = validation_set[1], validation_set[2]
 teX, teY = test_set[1], test_set[2]
 
-trX  = permutedims(reshape(trX,  (size(trX,1),  1, 28, 28)), [3,4,2,1])
-valX = permutedims(reshape(valX, (size(valX,1), 1, 28, 28)), [3,4,2,1])
-teX  = permutedims(reshape(teX,  (size(teX,1),  1, 28, 28)), [3,4,2,1])
-
 println("TrainSet: $(size(trX)) $(size(trY))")
 println("ValSet  : $(size(valX)) $(size(valY))")
 println("TestSet : $(size(teX)) $(size(teY))")
 
-graph = s()
+layerX, layerY, graph = build_model()
+opt = SgdOptimizerGraph(graph)
 
 epo_losses, epo_accu, val_losses, val_accu, all_losses = sgd(
-    net, (trX, trY), (valX, valY);
+    graph, layerX, layerY, opt, (trX, trY), (valX, valY);
     ttl_epo = 10, batch_size = batch_size,
     lrSchedule = x -> 0.01, verbose=1
 )
