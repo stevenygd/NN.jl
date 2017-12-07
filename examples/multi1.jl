f105d9c985fb7c3ad0dcc670773f4514867813de
Tiiiger <zty27x@gmail.com>
2017-12-07 00:42:30 -0500

    "play with @threads"

--- examples/multi1.jl
+++ examples/multi1.jl
@@ -1,14 +1,25 @@
-include("../src/NN.jl")
-using NN
+# include("../src/NN.jl")
+# using NN
 
-batch_size = 20
-layerX = InputLayer((batch_size,20))
-layerY = InputLayer((batch_size, 10))
-l1    = DenseLayer(layerX, 10; init_type = "Normal")
+batch_size = 2
+layerX = InputLayer((batch_size,2))
+layerY = InputLayer((batch_size, 2))
+l1    = DenseLayer(layerX, 2; init_type = "Uniform")
 l2    = SoftMaxCrossEntropyLoss(l1, layerY)
 
-l3 = DenseLayer(l1)
-println(size(l1.W))
+l1.W = zeros(l1.W)
+println(l1.W)
+println(l1.x)
+Threads.@threads for i = 1:8
+    l = DenseLayer(l1)
+    broadcast!(+, l.W, l.W, [1])
+    l.x = ones(l.x)
+end
+println(l1.W)
+println(l1.x)
+
+# l3 = DenseLayer(l1)
+# println(size(l1.W))
 
 # println(l1.W[1,:])
 # println(l3.W[1,:])
