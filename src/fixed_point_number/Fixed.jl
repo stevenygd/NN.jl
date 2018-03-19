d5fb8a647cd69d47239b902053e4055488e2b233
Tiiiger <zty27x@gmail.com>
2018-03-19 13:53:00 -0400

    "sketching ideas"

--- src/fixed_point_number/Fixed.jl
+++ src/fixed_point_number/Fixed.jl
@@ -8,17 +8,40 @@ import Base: ==, <, <=, -, +, *, /, ~, isapprox,
              div, fld, rem, mod, mod1, fld1, min, max, minmax,
              start, next, done, rand
 
-struct Fixed{T <: Signed,δ,f}
+struct Fixed{T <: Signed,f}
     i::T
 
     # constructor for manipulating the representation;
     # selected by passing an extra dummy argument
-    Fixed{T, δ, f}(i::Integer, _) where {T,δ,f} = new{T,δ,f}(i % T)
-    Fixed{T, δ, f}(x) where {T,δ,f} = convert(Fixed{T,δ,f}, x)
-    Fixed{T, δ, f}(x::Fixed{T,δ,f}) where {T,δ,f} = x
+    function Fixed{T, f}(i::Integer, _) where {T,f}
+        if i > typemax(T) new{T,f}(typemax(T))
+        elseif i < typemin(T) new{T,f}(typemin(T))
+        else new{T,f}(i)
+        end
+    end
+    Fixed{T,f}(x::Int) where {T,f} = convert(Fixed{T,f}, x)
+    Fixed{T,f}(x::AbstractFloat) where {T,f} = quantize(Fixed{T,f}, x)
+    Fixed{T,f}(x::Fixed{T,f}) where {T,f} = x
 end
 
-reinterpret(::Type{Fixed{T,δ,f}}, x::T) where {T <: Signed,δ,f} = Fixed{T,δ,f}(x, 0)
+struct BlockFixed{T<:Signed, σ}
+    i::T
+    function BlockFixed{T,σ}(i::Integer, _) where {T, σ}
+        if i > typemax(T) new{T,f}(typemax(T))
+        elseif i < typemin(T) new{T,f}(typemin(T))
+        else new{T,f}(i)
+        end
+    end
+end
+
+type BlockFixedArray{T<:Signed, σ}
+    A::Array{T}
+    function BlockFixedArray{T, σ}(A::Array{N}) where {T<:Signed, σ, N<:Integer}
+        new(A)
+    end
+end
+
+reinterpret(::Type{Fixed{T,f}}, x::T) where {T <: Signed,f} = Fixed{T,f}(x, 0)
 
 # helper for type
 widen1(::Type{Int8})   = Int16
@@ -36,10 +59,11 @@ bits_diff(::Type{Int16}, ::Type{Int32}) = 16
 bits_diff(::Type{Int32}, ::Type{Int64}) = 32
 
 # basic
-function zero(::Type{Fixed{T,σ,f}}) where {T<:Integer,σ,f}
-    Fixed{T,σ,f}(0,0)
+function zero(::Type{Fixed{T,f}}) where {T<:Integer,f}
+    Fixed{T,f}(0,0)
 end
-# multiplication
+
+# quantize
 function qround(T::Type{<:Integer}, x::Integer)
     if x > typemax(T)
         return typemax(T)
@@ -56,32 +80,30 @@ function qround(T::Type{<:Integer}, x::Integer)
     end
 end
 
-# convert
-function convert(::Type{Fixed{T, σ, f}}, x::AbstractFloat) where {T<:Integer, σ, f}
+function quantize(::Type{Fixed{T, f}}, x::AbstractFloat) where {T<:Integer,f}
     # x /= σ
     if x >= (typemax(T)+1)>>f
         println("x is $x")
-        return Fixed{T,σ,f}(typemax(T),0)
+        return Fixed{T,f}(typemax(T),0)
     elseif x < typemin(T)>>f
-        return Fixed{T,σ,f}(typemin(T),0)
+        return Fixed{T,f}(typemin(T),0)
     else
         x_floor = floor(T, trunc(widen1(T),x)<<f + rem(x,1)*(one(widen1(T))<<f))
         r = x*2^f - x_floor
-        println("r is $r")
         p = rand()
-        println("p is $p")
         if p > r
-            return Fixed{T,σ,f}(x_floor+1, 0)
-        else return Fixed{T,σ,f}(x_floor, 0)
+            return Fixed{T,f}(x_floor+1, 0)
+        else return Fixed{T,f}(x_floor, 0)
         end
     end
 end
 
-function array_cast(fix::Type{Fixed{T,σ,f}}, A::Array{<:AbstractFloat}) where {T<:Integer,σ,f}
-    B = zeros(A, Fixed{T,σ,f})
+function array_quantize(fix::Type{Fixed{T,f}}, A::Array{<:AbstractFloat}) where {T<:Integer,f}
+    B = zeros(A, Fixed{T,f})
     for i=1:length(A)
-        B[i] = Fixed{T,σ,f}(A[i])
+        B[i] = Fixed{T,f}(A[i])
     end
     return B
 end
+
 # convert(::Type{Fixed{T,δ,f}}, x::Integer) where {T,σ,f} = Fixed{T,f}(round(T, convert(widen1(T),x/δ)<<f),0)
